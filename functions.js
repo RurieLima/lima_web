9ac140e89b5fc6b4c62b3835b93cb0af10a90393
Rurie Lima <149429013+RurieLima@users.noreply.github.com>
2024-09-04 16:42:11 +0200

    Add files via upload
    init lima web

---
+++ functions.js
@@ -0,0 +1,68 @@
+// Função para criar o efeito de spray no body
+document.addEventListener('mousemove', function(e) {
+    const wave = document.createElement('div');
+    wave.className = 'wave';
+    document.body.appendChild(wave);
+
+    const x = e.clientX;
+    const y = e.clientY;
+
+    wave.style.left = `${x}px`;
+    wave.style.top = `${y}px`;
+
+    wave.addEventListener('animationend', () => {
+        wave.remove(); // Remove o spray após a animação terminar
+    });
+});
+
+// Função para criar o efeito de onda nas letras
+function createLetterWaveEffect(element) {
+    const text = element.innerText;
+    element.innerHTML = '';
+
+    // Dividir o texto em caracteres (incluindo espaços)
+    text.split('').forEach((char, index) => {
+        const span = document.createElement('span');
+        // Verificar se o caractere é um espaço
+        if (char === ' ') {
+            span.innerHTML = '&nbsp;'; // Utiliza espaço não quebrável para garantir que o espaço seja mantido
+        } else {
+            span.innerText = char;
+            span.classList.add('letter-wave');
+        }
+        element.appendChild(span);
+
+        // Adicionar um evento para o efeito de onda
+        span.addEventListener('mouseover', () => {
+            if (char !== ' ') {  // Não aplica o efeito de onda nos espaços
+                span.classList.add('color-change');  // Adiciona a classe para mudança de cor
+                span.style.transform = 'scale(1.5)';
+                // Remover o efeito após a transição
+                setTimeout(() => {
+                    span.style.transform = 'scale(1)';
+                    span.classList.remove('color-change');  // Remove a classe de cor
+                }, 300);
+            }
+        });
+    });
+}
+
+// Aplicar o efeito a todos os h1, h2 e h3
+document.querySelectorAll('h1, h2, h3').forEach(createLetterWaveEffect);
+
+// Função para criar o efeito de onda
+function createWaveEffect(element) {
+    element.style.display = 'inline-block';
+    element.style.transition = 'transform 5s ease-out';
+
+    // Adicionar um evento para o efeito de onda
+    element.addEventListener('mouseover', () => {
+        element.style.transform = 'rotateY(1080deg)';
+        setTimeout(() => {
+            element.style.transform = 'rotateY(720deg)';
+        }, 2000);
+    });
+}
+
+// Aplicar o efeito a todos as img, e i
+document.querySelectorAll('img, i').forEach(createWaveEffect);
---
+++ readme.md
@@ -0,0 +1,24 @@
+<div align="center">
+
+  <img src="img/limaweblogonew.png" alt="Logo lima web" width="200">
+
+# Lima Web
+
+</div>
+
+### Tools
+
+[![Js](https://img.shields.io/badge/logo-javascript-blue?logo=javascript)](https://developer.mozilla.org/es/docs/Web/JavaScript)
+
+[![Html](https://img.shields.io/badge/Html-red)](https://developer.mozilla.org/es/docs/Web/CSS)
+
+[![Css](https://img.shields.io/badge/Css-blue)](https://developer.mozilla.org/es/docs/Web/CSS)
+
+
+### Contact
+
+[![Linkedin](https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/rurie-lima/)
+
+#### Author
+
+- [@rurielima](https://www.github.com/RurieLima)
